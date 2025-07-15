d9e6ad1be230dd843e8b6a32b5ca99046bb3b644
viewtifultiger <viewtifultiger@gmail.com>
2025-07-15 16:27:22 -0700

    chore: updated cleanInventory()

--- _config_cleanInventory.lua
+++ _config_cleanInventory.lua
@@ -1,9 +1,14 @@
 return {
 	["minecraft:cobblestone"] = true,
 	["minecraft:andesite"] = true,
+	["minecraft:gravel"] = true,
+	["minecraft:dripstone_block"] = true,
+	["minecraft:pointed_dripstone"] = true,
 	["minecraft:granite"] = true,
 	["minecraft:diorite"] = true,
 	["minecraft:dirt"] = true,
 	["minecraft:cobbled_deepslate"] = true,
+	["minecraft:deepslate_bricks"] = true,
+	["minecraft:cracked_deepslate_bricks"] = true,
 	["minecraft:tuff"] = true,
 }
