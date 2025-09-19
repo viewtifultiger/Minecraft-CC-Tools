19fdc4f738c162f1a02059076f7f9915e9c05d40
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2025-09-18 19:28:43 -0700

    feat: select empty slot

--- turtletools.lua
+++ turtletools.lua
@@ -39,7 +39,14 @@ function M.selectItem(item)
 	end
 	return false
 end
-
+function M.selectEmptySlot()
+	for slot = 1,16 do
+		if turtle.getItemCount(slot) == 0 then
+			turtle.select(slot)
+			return true
+		end
+	end
+end
 
 -- DIG FUNCTIONS --
 
