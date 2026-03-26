ba9a4bafa4a17d35ae979c0a98f3a28f81c50e76
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2026-03-26 14:58:01 -0700

    docs(dig_2x2_hole): updating comments

--- dig_2x2_hole.lua
+++ dig_2x2_hole.lua
@@ -1,3 +1,24 @@
+--[[
+	-- NOTES --
+	
+	
+	-- TO DO --
+-- 1 create a UI to set the Y level, iterations, orientation, torch placement
+-- 2 automate the number of iterations performed by making calculations based on fuel level and Y level
+-- 3 create a way to track fuel level and refuel automatically
+-- 5 check other scripts that use dig_2x2_hole and update the use of the boolean direction value
+-- 6 have the hole.dig() function return a boolean (true or false based on horizontal position and the turtle's proximity), and a tabl
+--	of information that includes blocks that were dug
+-- 7 find a way to have the turtle mine blocks around even though it found an invalid block in the last position of the dig loop
+
+
+	-- DEBUG NOTES--
+
+	
+	-- Working on: 
+		
+]]
+
 local hole = require("hole_2x2")
 local ct = require("codetools")
 local tt = require("turtletools")
@@ -21,64 +42,19 @@ print("Fuel Level: ", currentFuel)
 io.write("Enter number of iterations: ")
 local iterations = tonumber(io.read())
 
-local cost = 3 * iterations * (y + 65)
--- the above calculation is the simplified version of the below:
--- max_y * 2 -> cost to mine down and dig
--- + 1 -> first dig down
--- + may_y -> cost to return to surface from bedrock
--- * iterations -> number of times to loop
--- + (2 * iterations) max number of moves to move to next section
+-- 3 moves per level (going down/going up/sidemovement) * number of levels (current + 63 below y=0 + 2 for moving to next iteration) * total iterations 
+local max_cost = 3 * (y + 65) * iterations
 local startingFuel = currentFuel
 
-if startingFuel < cost then
+if startingFuel < max_cost then
 	print("You don't have enough fuel")
 	return
 else
-	print("Maximum cost: " .. cost)
+	print("Maximum cost: " .. max_cost)
 end
 
 local placeTorches = ct.verifyTorchDownSetting(false)
 
---[[
-	-- NOTES --
-	currently I want to figure out a way to pass a dictionary to the main dig function, however I'm not sure what will happen to this 
-		upper level 2x2 digging program. I can imagine a few digging scripts that may reuse the 2x2 dig hole function, but in that case
-		I would want to pass the hole type as part of an option within the dictionary. Therefore this upper level program would
-		be obsolete. In order to generalize the 2x2 hole, we would need to take the first iteration of digging a 2x2 square. 
-		from there we can modify or move the turtle to create more 2x2 squares in other directions.
-	
-	
-	-- TO DO --
--- 1 create a UI to set the Y level, iterations, orientation, torch placement
--- 2 automate the number of iterations performed by making calculations based on fuel level and Y level
--- 3 create a way to track fuel level and refuel automatically
--- 5 check other scripts that use dig_2x2_hole and update the use of the boolean direction value
--- 6 have the hole.dig() function return a boolean (true or false based on horizontal position and the turtle's proximity), and a tabl
---	of information that includes blocks that were dug
--- 7 find a way to have the turtle mine blocks around even though it found an invalid block in the last position of the dig loop
--- 8 find a way to give the main dig funtion a dictionary of options and have the function handle those options
--- 9 let the M.dig() function accept a blacklist of digging blocks
-
-	-- DEBUG NOTES--
-
-	
-	-- Working on: 
-	-- making sure opposite turn works when the turtle goes to the right
-	-- figuring out other use cases for the hole.dig() function and how to generalize it
-	
-		
-
-			
-		@variable goal boolean: direction of new holes to be dug; set by the user at the start of the program; 
-		true -> start the next new hole to the right
-		false -> start the next new hole to the left
-		
-		Mhole.dig() [MAIN DIGGING FUNCTION]: returns a boolean that represents the horizontal_position of the turtle on the 2x2 grid (left or right half);
-		we compare this to the direction of the next_hole_direction to determine how many spaces to move to the next hole.
-		true -> will start the next new hole to the right
-		false -> will start the next new hole to the left
-		
-]]
 
 local horizontal_position
 for i=1,iterations do
