1df4035f79f902bb6e3045c87c5d4db8c8ea12ca
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2026-04-04 15:22:16 -0700

    chore: update working on

--- dig_2x2_hole.lua
+++ dig_2x2_hole.lua
@@ -17,9 +17,8 @@
 	-- DEBUG NOTES--
 		-- find out why the same item can end up in different item slots even though the stacks are not full
 	
-	-- Working on: 
-		-- check movement functions with new checkers added
-		-- remove error checks in the local move function
+	-- Working on:
+	-- check the dig_2x2_hole script to see if any new features should be added before creating a hole_2x2.dig_hole_up()
 ]]
 
 local horizontal_2x2 = require("hole_2x2")
