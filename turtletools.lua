de511a11e975e3ab4603a8dee84316ef36a6c074
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2026-04-04 12:50:53 -0700

    chore(turtletools): remove unused comments

--- turtletools.lua
+++ turtletools.lua
@@ -13,28 +13,22 @@ local M = {}
 ----------------PUBLIC-FUNCTIONS-----------------------------------------------------------------------------------------------------------------------
 --[[
 	vertical_direction string (must be "forward", "up", or "down"): ; context table: context_builder.create() or similar and must have a blacklist
-
-	WARNING: NEEDS A VALIDATE_BLACKLIST FUNCTION OR ELSE ANY TABLE WILL PASS (can check for proper keys and type(values) == "boolean"
 ]]
 function M.inspect_if_blacklisted(vertical_direction, context) --> --> success boolean: if block present and not blacklisted; table block_data | nil (empty block)
 	direct.vertical_direction_checker(vertical_direction, 3)
 	context = context or context_builder.create()
 	context_builder.run_checks(context, {"dig_config", "blacklist"}, 3)
-	--!!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	return dig_core.inspect_if_blacklisted(vertical_direction, context.dig_config.blacklist)
 end
 --[[
 	vertical_direction string (must be "forward", "up", or "down"): ; context table: context_builder.create() or similar and must have a blacklist 
 		and must have stats
-
-	WARNING: NEEDS A VALIDATE_BLACKLIST FUNCTION OR ELSE ANY TABLE WILL PASS (can check for proper keys and type(values) == "boolean"
 ]]
 function M.try_dig(vertical_direction, context) --> boolean: if block was dug; table | nil (if empty block); string reason for the returned boolean
 	direct.vertical_direction_checker(vertical_direction, 3)
 	context = context
 	context_builder.run_checks(context, {"basic_structure", "stats", "blocks_mined",
 											"blocks_mined_by_name", "blacklist"}, 3) --check state, dig_config, blacklist, stats, blocks_mined, blocks_mined by name
-	--!!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	return dig_core.try_dig(vertical_direction, context)
 end
 
