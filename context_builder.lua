436d3b7a66b1703787427b49f1a310e04c2e2520
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2026-04-04 12:45:02 -0700

    feat(context): add more checkers
    -- state
    -- facing
    -- fuel
    -- position
    -- position.x
    -- position.y
    -- position.z
    -- total_moves
    -- dig_config

--- context_builder.lua
+++ context_builder.lua
@@ -11,16 +11,9 @@ function M.create(overrides)
         dig_config = overrides.options or dig_config.create()
     }
 end
-
 --[[
     NOTES:
-        we don't entirely validate the base context within context_checker. Sometimes we only want to validate a particular set of keys. We might
-        want to split the context_checker into smaller functions for each key check. We can then create custom checkers depending on what is
-        needed, such as a base checker or a state, state.stats, state.stats.blocks_mined_by_name and so on. 
-        checking everything at once would be unecessary
 ]]
-
-
 local function basic_structure_checker(context, level)
 	if type(context.state) ~= "table" then
 		error('missing context.state; "' .. tostring(context.state) .. '" is not a table', level or 2)
@@ -29,16 +22,65 @@ local function basic_structure_checker(context, level)
 		error('missing context.dig_config; "' .. tostring(context.dig_config) .. '" is not a table', level or 2)
 	end
 end
-local function dig_config_checker(context, level)
-	if type(context.dig_config) ~= "table" then
-		error('missing context.dig_config; "' .. tostring(context.dig_config) .. '" is not a table', level or 2)
+--------------------STATE----------------------------------------------------
+local function state_checker(context, level)
+	if type(context.state) ~= "table" then
+		error('missing context.state; "' .. tostring(context.state) .. '" is not a table', level or 2)
 	end
+end
+local function facing_checker(context, level)
+    local facing = context.state.facing
+    if type(facing) ~= "string" then
+        error('context missing state.facing; "' .. tostring(facing) .. '" is not a string', level or 2)
+    end
+end
+local function fuel_checker(context, level)
+    local fuel = context.state.fuel
+    if type(fuel) ~= "number" then
+        error('context missing state.fuel; "' .. tostring(fuel) .. '" is not a number', level or 2)
+    end
+end
+local function position_checker(context, level)
+    local position = context.state.position
+    if type(position) ~= "table" then
+        error('context missing state.position; "' .. tostring(position) .. '" is not a table', level or 2)
+    end
+end
+local function position_x_checker(context, level)
+    local position = context.state.position.x
+    if type(position) ~= "number" then
+        error('invalid  state.position.x; "' .. tostring(position) .. '" is not a number', level or 2)
+    end
+end
+local function position_y_checker(context, level)
+    local position = context.state.position.y
+    if type(position) ~= "number" then
+        error('invalid  state.position.y; "' .. tostring(position) .. '" is not a number', level or 2)
+    end
+end
+local function position_z_checker(context, level)
+    local position = context.state.position.z
+    if type(position) ~= "number" then
+        error('invalid  state.position.z; "' .. tostring(position) .. '" is not a number', level or 2)
+    end
 end
 local function stats_checker(context, level)
     local stats = context.state.stats
     if type(stats) ~= "table" then
         error('context missing state.stats; "' .. tostring(stats) .. '" is not a table', level or 2)
     end
+end
+local function total_moves_checker(context, level)
+    local total_moves = context.state.stats.total_moves
+    if type(total_moves) ~= "number" then
+        error('state missing stats.total_moves; "' .. tostring(total_moves) .. '" is not a number', level or 2)
+    end
+end
+-------------------DIG_CONFIG-------------------------------------------------
+local function dig_config_checker(context, level)
+	if type(context.dig_config) ~= "table" then
+		error('missing context.dig_config; "' .. tostring(context.dig_config) .. '" is not a table', level or 2)
+	end
 end
 local function blocks_mined_checker(context, level)
     local blocks_mined = context.state.stats.blocks_mined
@@ -61,10 +103,18 @@ end
 
 local CHECKS = {
     ["basic_structure"] = basic_structure_checker,
-    ["dig_config"] = dig_config_checker,
+    ["state"] = state_checker,
+    ["facing"]= facing_checker,
+    ["fuel"] = fuel_checker,
+    ["position"] = position_checker,
+    ["position_x"] = position_x_checker,
+    ["position_y"] = position_y_checker,
+    ["position_z"] = position_z_checker,
     ["stats"] = stats_checker,
+    ["total_moves"] = total_moves_checker,
     ["blocks_mined"] = blocks_mined_checker,
     ["blocks_mined_by_name"] = blocks_mined_by_name_checker,
+    ["dig_config"] = dig_config_checker,
     ["blacklist"] = blacklist_checker
 }
 function M.run_checks(context, checks, level)
