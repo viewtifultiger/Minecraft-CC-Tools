07df5e0d9d0ac5e7bd548b9bc2f05e5a6f571398
viewtifultiger <124817262+viewtifultiger@users.noreply.github.com>
2026-04-09 16:38:05 -0700

    refactor(movement_core): shorten the movement and turn logic

--- movement_core.lua
+++ movement_core.lua
@@ -21,30 +21,27 @@ function M.move(direction, state) --> boolean Whether the turtle could successfu
     local directions = direct.MOVEMENT_DIRECTIONS
     local success, reason = movement()
 
-    if not success then
-        return false, reason
-    elseif direction == directions.UP or direction == directions.DOWN then
-        position.y = position.y + vectors[direction].y
-        state.depth = state.depth + (-1 * vectors[direction].y)
-    else
-        position.x = position.x + (direction == directions.FORWARD and vectors[state.facing].x or -1 * (vectors[state.facing].x))
-        position.z = position.z + (direction == directions.FORWARD and vectors[state.facing].z or -1 * (vectors[state.facing].z))
+    if success then
+        if direction == directions.UP or direction == directions.DOWN then
+            position.y = position.y + vectors[direction].y
+            state.depth = state.depth + (-1 * vectors[direction].y)
+        else
+            position.x = position.x + (direction == directions.FORWARD and vectors[state.facing].x or -1 * (vectors[state.facing].x))
+            position.z = position.z + (direction == directions.FORWARD and vectors[state.facing].z or -1 * (vectors[state.facing].z))            
+        end
+        state.fuel = state.fuel - 1
+        state.stats.total_moves = state.stats.total_moves + 1
     end
-
-    state.fuel = state.fuel - 1
-    state.stats.total_moves = state.stats.total_moves + 1
-    return true, nil
+    return success, reason
 end
 
 function M.turn(direction, state) --> boolean Whether the turtle could succesfully turn, string | nil The reason the turtle could not turn
     local success, reason = turn_functions[direction]()
-    if not success then
-        return false, reason
-    else
+    if success then
         state.facing =
         ((direction == direct.TURN_DIRECTIONS.LEFT) and direct.LEFT_TURN[state.facing]) or direct.RIGHT_TURN[state.facing]
     end
-    return true, nil
+    return success, reason
 end
 
 return M
